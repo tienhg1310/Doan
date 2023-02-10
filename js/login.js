a4c1cc876d4230ca68ef2590fd9c71cc0832a265
Tienhg2001 <tienhg201@gmail.com>
2023-02-10 14:57:15 +0700

    edit

--- js/login.js
+++ js/login.js
@@ -1,14 +1,18 @@
 window.onload = function() {
-	var tabControlBtn = document.querySelectorAll(".tab-control-btn");
-	var tabFormLogin = document.querySelector(".login-form");
-	var tabFormRegister = document.querySelector(".register-form");
-	var imageFile = document.querySelector(".image-profile");
+	var tabControlBtn = document.querySelectorAll(".tab-control-btn"); //lấy tất cả các nút
+	var tabFormLogin = document.querySelector(".login-form"); //lấy nút login
+	var tabFormRegister = document.querySelector(".register-form"); //lấy nút register
 
 
+	// lặp qua từng nút 
 	tabControlBtn.forEach(function(element, index) {
+		// thêm sự kiện click cho từng nút lặp qua
 		element.addEventListener("click", function() {
+			// nếu nút đó tồn tại class là login 
 			if (element.classList.contains("login")) {
+				// thêm class active vào nút login
 				tabFormLogin.classList.add("active");
+				// xoá class active ở nút register
 				tabFormRegister.classList.remove("active");
 			} else {
 				tabFormRegister.classList.add("active");
@@ -21,15 +25,5 @@ window.onload = function() {
 		});
 	});
 
-	document.querySelector(".image-file").addEventListener("change", function(e) {
-		imageFile.src = URL.createObjectURL(e.target.files[0]);
-	});
-
-	document.querySelector(".gender-select").addEventListener("change", function(e) {
-		if (e.target.value == "true") {
-			document.querySelector(".image-profile").src = window.location.origin + "/user-male.jpg";
-		} else {
-			document.querySelector(".image-profile").src = window.location.origin + "/user-female.jpg";
-		}
-	});
+	
 }
